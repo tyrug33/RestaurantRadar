607d7026747347fd8ef9c5fd81e7455a033115a9
jpatag <jjromr@gmail.com>
2024-04-24 13:51:47 -0400

    Cleaned up server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const UserModel = require("./models/Users");
+// const UserModel = require("./models/Users");
 const RestaurantModel = require("./models/Restaurants");
 
 const cors = require("cors");
@@ -13,28 +13,28 @@ mongoose.connect(
     "mongodb+srv://restaurantradar:restaurantradar@cluster0.twyqru6.mongodb.net/restaurantradar?retryWrites=true&w=majority&appName=Cluster0"
   );
 
-  app.get("/getUsers", (req, res) => {
-    UserModel.find({}).exec()
-        .then(result => {
-            if (result.length === 0) {
-                res.status(404).json({ message: "No users found" });
-            } else {
-                res.status(200).json(result);
-            }
-        })
-        .catch(err => {
-            console.error("Error fetching users:", err);
-            res.status(500).json({ message: "Internal server error" });
-        });
-});
+//   app.get("/getUsers", (req, res) => {
+//     UserModel.find({}).exec()
+//         .then(result => {
+//             if (result.length === 0) {
+//                 res.status(404).json({ message: "No users found" });
+//             } else {
+//                 res.status(200).json(result);
+//             }
+//         })
+//         .catch(err => {
+//             console.error("Error fetching users:", err);
+//             res.status(500).json({ message: "Internal server error" });
+//         });
+// });
 
-app.post("/createUser", async (req, res) => {
-    const user = req.body;
-    const newUser = new UserModel(user);
-    await newUser.save();
+// app.post("/createUser", async (req, res) => {
+//     const user = req.body;
+//     const newUser = new UserModel(user);
+//     await newUser.save();
   
-    res.json(user);
-  });
+//     res.json(user);
+//   });
 
 app.listen(3001, () => {
     console.log("SERVER RUNS!");
