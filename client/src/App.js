e0f228fa27218c7086acea11971d01a1efd48ed5
jpatag <jjromr@gmail.com>
2024-04-24 13:44:18 -0400

    Added page and fixed REST api requests

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './Home'; // Import your home page component
 import Login from './Login'; // Import your login page component
+import Review from './Reviews';
+
 
 const App = () => {
   return (
@@ -11,6 +13,7 @@ const App = () => {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Home />} /> {/* Home page route */}
+          <Route path="/reviews" element={<Review />} />
         </Routes>
       </div>
     </Router>
